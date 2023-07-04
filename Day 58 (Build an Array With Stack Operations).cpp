e342eaed3cb5f91b7c23a0d8789f6b05016575bb
Udittyagi07 <103762087+Udittyagi07@users.noreply.github.com>
2023-07-05 00:08:48 +0530

    Create Day 58 (Build an Array With Stack Operations).cpp

---
+++ Day 58 (Build an Array With Stack Operations).cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans;
+        int i=0;
+        int count=1;
+        while(i<target.size())
+        {
+            if(target[i]==count)
+            {
+                ans.push_back("Push");
+                i++;
+                count++;
+            }
+            else
+            {
+                ans.push_back("Push");
+                ans.push_back("Pop");
+                count++;
+            }
+        }
+        return ans;
+    }
+};
