cfd15207924bcda4c3a6bf2d289fc950e20899c3
Udittyagi07 <103762087+Udittyagi07@users.noreply.github.com>
2023-08-13 00:24:07 +0530

    Create Day 97 (Removing Stars From a String).cpp

---
+++ Day 97 (Removing Stars From a String).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans="";
+        int i=0;
+        while(i<s.size())
+        {
+            if(s[i]=='*')
+            {
+                ans.pop_back();
+            }
+            else
+            {
+                ans.push_back(s[i]);
+            }
+            i++;
+        }
+        return ans;
+    }
+};
